96f0ecb913d62ec30e684148358bf9f072fe4e6c
ajey108 <ajayxa1@gmail.com>
2024-07-18 22:47:51 +0530

    Update authcontroller.js

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -1,4 +1,27 @@
-export const SignUp = (req,res)=>{
-    res.send('signup')
-    console.log('signup user');
-}
+export const SignUp = async (req,res)=>{
+   try{
+    const {
+        fullName, username,password,confirmPassword,gender } = req.body;
+
+        if(password !== confirmPassword){
+            return res.status(400).json({error:"Passwords dont match"})
+        }
+        
+        const user = await User.findOne({username});
+
+
+        if(user){
+            return res.status(400).json({error:"Username already exists"})
+        }
+
+        //Hash password
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        const newUser = new User({
+            
+        })
+
+   } catch(error){}
+};
