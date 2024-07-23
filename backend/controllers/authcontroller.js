8c9f2d24bfa77d20049c45edb758a3a3710f2da4
ajey108 <ajayxa1@gmail.com>
2024-07-23 23:27:41 +0530

    Update authcontroller.js

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -44,7 +44,7 @@ export const SignUp = async (req,res)=>{
      if(newUser){
 
         //Generate JWT token
-         generateTokenAndSetCookie(newUser._id,res)
+         generateTokenAndSetCookie(newUser._id,res);
 
 
         await newUser.save();
@@ -64,3 +64,38 @@ export const SignUp = async (req,res)=>{
     res.status(500).json({error:"InternalServerError"})
    }
 };
+
+
+export const login = async (req,res)=>{
+
+   try{
+    const {username,password} = req.body;
+
+    const user = await User.findOne({username});
+    //compare the input passw & db passw
+    const isPasswordCorrect = await bcrypt.compare(password,user?.password || "");
+    if(!user || !isPasswordCorrect){
+        return res.status(400).json({error:"Invalid username or password"});
+
+    }
+
+    generateTokenAndSetCookie(user._id,res);
+    res.status(200).json({
+        _id:user._id,
+        fullName:user.fullName,
+        username:user.username,
+        profilePic:user.profilePic,
+    });
+
+
+   }catch(error){
+    console.log("error in login controller",error.message);
+    res.status(500).json({error:"internal Server Error"});
+   };
+}
+
+
+export const logout =(req,res)=>{
+
+    console.log("logout");
+}
