aabbb88a5a9010e9a498b1fec8415e777a7fbae4
ajey108 <ajayxa1@gmail.com>
2024-07-23 23:27:27 +0530

    Update authroute.js

--- backend/routes/authroute.js
+++ backend/routes/authroute.js
@@ -1,8 +1,10 @@
 import express from "express";
-import {  SignUp } from "../controllers/authcontroller.js";
+import {  SignUp,login,logout } from "../controllers/authcontroller.js";
 
 const router = express.Router();
 
 router.post('/signup',SignUp);
+router.post('/login',login);
+router.post('/logout',logout);
 
 export default router;
