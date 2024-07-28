129bf00f266b680ca8121adb1eda187f9f1a69ce
ajey108 <ajayxa1@gmail.com>
2024-07-28 22:04:41 +0530

    Add message.controller.js

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,10 @@
+export const sendMessage = async (req,res)=>{
+try{
+const {message} = req.body;
+const {id} = req.params;
+const senderId = req.userId
+} catch(error){
+    console.log("Error in sendMessage controller:",error.message)
+    res.status(500).json({error:"Internal server error"});
+}
+};
