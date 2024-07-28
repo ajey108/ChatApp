bb5bd17e8713056d7b1ab92dd6b2c72298b5874b
ajey108 <ajayxa1@gmail.com>
2024-07-28 22:04:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 import express from 'express';
 import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
+
 import authRoutes from './routes/authroute.js'
+import messageRoutes from './routes/message.route.js'
 import connectToMongoDb from './db/connectMongoDb.js';
 
 const app = express();
 const PORT = process.env.PORT|| 5000;
 dotenv.config();
 
-app.use(express.json());
-app.use('/api/auth',authRoutes)
+//middleware
+app.use(express.json());// parse incoming req with JSON payloads(req.body)
+app.use(cookieParser());// parese incoming cookies from req.cookies
+
+
+app.use('/api/auth',authRoutes);
+app.use('/api/messages',messageRoutes);
 
 
 // app.get('/',(req,res)=>{
