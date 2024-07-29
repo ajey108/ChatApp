81c52c8f7c951737799ae5b160b767f71ba6fd01
ajey108 <ajayxa1@gmail.com>
2024-07-29 21:50:38 +0530

    Update protechRoute.js

--- backend/middleware/protectRoute.js
+++ backend/middleware/protectRoute.js
@@ -1,6 +1,7 @@
 import jwt from 'jsonwebtoken';
+import User from '../models/usermodel.js';
 
-const protectRoute = (req,res,next)=>{
+const protectRoute = async (req,res,next)=>{
     try{
         //get the token from the cookies
         const token = req.cookies.jwt;
@@ -11,10 +12,22 @@ const protectRoute = (req,res,next)=>{
         //if there is token
          const decoded = jwt.verify(token,process.env.JWT_SECRET);
 
+        // there is token but invalid
          if(!decoded){
            return res.status(401).json({error:"Unauthorized - Invalid Token"}); 
          }
+
+         const user = await User.findById(decoded.userId).select("-password");
+         if(!user){
+            return res.status(404).json({error:"User not found"});
+         }
+
+         req.user = user
+
+         next(); // to run sendMessage function after checks
     }catch(error){
         console.log("Error in protectRoute middleware:",error.message)
         res.status(500).json({error:"Internal Server Error"});
     }};
+
+    export default protectRoute;
