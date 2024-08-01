3620d95d2a46119bc790bb6fe2465423c45f94b7
ajey108 <ajayxa1@gmail.com>
2024-08-01 16:49:59 +0530

    Update message.controller.js

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,43 +1,42 @@
-import Conversation from '../models/conversation.model.js'
+import Conversation from '../models/conversation.model.js';
+import Message from '../models/message.model.js';
 
+export const sendMessage = async (req, res) => {
+  try {
+    // message as input from the user
+    const { message } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
 
-export const sendMessage = async (req,res)=>{
-try{
-const {message} = req.body;
-const {id:receiverId} = req.params;
-const senderId = req.user._id
+    // find the conversation between the users
+    let conversation = await Conversation.findOne({
+      participants: { $all: [senderId, receiverId] },
+    });
 
-//to find the conversation between the users
+    // create the conversation if it doesn't exist
+    if (!conversation) {
+      conversation = await Conversation.create({
+        participants: [senderId, receiverId],
+      });
+    }
 
-let conversation = await Conversation.findOne({
-    participants:{$all:[senderId,receiverId]},
-})
+    // create the new message
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      message,
+    });
 
-//create the conversation
-if(!conversation){
-    conversation = await Conversation.create({
-        participants:[senderId,receiverId],
+    // save the new message to the database
+    await newMessage.save();
 
-    })
-}
-
-const newMessage = new MessageChannel({
-    senderId,
-    receiverId,
-    message,
-})
-
-
-//if there is new message
-if(newMessage){
+    // add the message to the conversation and save it
     conversation.messages.push(newMessage._id);
-}
+    await conversation.save();
 
-res.status(201).json(newMessage);
-
-
-} catch(error){
-    console.log("Error in sendMessage controller:",error.message)
-    res.status(500).json({error:"Internal server error"});
-}
+    res.status(201).json(newMessage);
+  } catch (error) {
+    console.error('Error in sendMessage controller:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 };
