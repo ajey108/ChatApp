65da0691cff5b88071eaff96d9dbc3d1f5272d58
ajey108 <ajayxa1@gmail.com>
2024-08-16 08:15:06 +0530

    Update message.model.js

--- backend/models/message.model.js
+++ backend/models/message.model.js
@@ -1,20 +1,22 @@
-import { request } from "express";
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema({
-    senderId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        require:true
+    senderId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
     },
-    receiverId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        request:true
+    receiverId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
     }
-},{timeseries:true});
+}, { timestamps: true });
 
-
-const Message = mongoose.model("Message",messageSchema);
+const Message = mongoose.model("Message", messageSchema);
 
 export default Message;
