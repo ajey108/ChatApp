34246b6ba75b3acd4c44c34d39b460ff38e2d657
ajey108 <ajayxa1@gmail.com>
2024-08-16 10:21:29 +0530

    Add user.route.js

---
+++ backend/routes/user.route.js
@@ -0,0 +1,8 @@
+import express from "express";
+import protectRoute from "../middleware/protectRoute.js";
+import { getUsersForSiderbar } from "../controllers/user.controller.js";
+
+const router = express.Router();
+
+router.get('/',protectRoute,getUsersForSiderbar)
+export default router;
