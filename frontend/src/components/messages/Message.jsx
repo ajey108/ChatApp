2a12334bcfd8be50e21b404a2833888972747c34
ajey108 <ajayxa1@gmail.com>
2024-08-21 11:21:41 +0530

    Update Message UI

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const Message = () => {
     return (
-        <div className='chat chat-end'>
-            <div className='chat-image avatar'>
-                <div className="w-10 rounded-full">
-                    <img src={"https://img.freepik.com/premium-photo/avatar-resourcing-company_1254967-6653.jpg?w=740"} />
+        <div className="chat chat-end">
+            <div className="chat-image avatar">
+                <div className="w-16 md:w-20 rounded-full">
+                    <img src="https://img.freepik.com/premium-photo/avatar-resourcing-company_1254967-6653.jpg?w=740" alt="Avatar" />
                 </div>
-                <div className={"chat-bubble text-white bg-blue-500"}></div>
+            </div>
+            <div className="chat-bubble text-white bg-blue-500">
+                Hi, what's up?
+            </div>
+            <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
+                10:00 am
             </div>
         </div>
-    )
+    );
 }
 
-export default Message
+export default Message;
--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -3,8 +3,18 @@ import Messages from './Messages'
 
 const MessageContainer = () => {
   return (
-    <div className=''>
+    <div className='md:min-w-[450px] flex flex-col'>
+        <>
+    <div className="bg-slate-500 px-4 py-2 mb-2">
+        <span className='label-text'>To:</span> <span className='text-gray-900 font-bold'>John Snow</span>
+    </div>
+
+
+
         <Messages/>
+        
+        </>
+      
     </div>
   )
 }
