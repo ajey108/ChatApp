e6d4533dafa4ddfc1a2455d93d50bac50333abc7
ajey108 <ajayxa1@gmail.com>
2024-08-21 11:10:11 +0530

    Add LogoutButton.jsx

---
+++ frontend/src/components/LogoutButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BiLogOutCircle } from "react-icons/bi";
+
+const LogoutButton = () => {
+  return (
+    <div className='mt-auto '>
+        <BiLogOutCircle/>
+    </div>
+  )
+}
+
+export default LogoutButton
