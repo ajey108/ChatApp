c76ffc928c8cf563b288a2f5c767741a217fd5d1
ajey108 <ajayxa1@gmail.com>
2024-08-25 10:28:24 +0530

    Update Logout component

--- frontend/src/components/LogoutButton.jsx
+++ frontend/src/components/LogoutButton.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { BiLogOutCircle } from "react-icons/bi";
+import useLogout from '../hooks/useLogout';
 
 const LogoutButton = () => {
+  const {loading,logout} = useLogout();
   return (
     <div className='mt-auto '>
-        <BiLogOutCircle/>
+        {!loading ? (
+          <BiLogOutCircle onClick={logout}/>
+        ): (<span className='loading  loading-spinner'></span>)}
     </div>
   )
 }
