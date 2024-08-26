99f10ea5f025a602b7d5da963711f5f83fe0c0c0
ajey108 <ajayxa1@gmail.com>
2024-08-26 09:54:53 +0530

    Add Zustnad for statemanagement, Add usLogin.js and useConversaton hooks

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,45 +1,66 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import { useState} from "react";
+import { Link } from "react-router-dom";
+import useLogin from '../hooks/useLogin'
+
 
 const Login = () => {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-slate-700 mx-auto p-4">
-            <h1 className='text-3xl font-semibold text-center text-gray-400 mb-6'>Log In</h1>
-            <form className='w-full max-w-xs'>
-                <div className='mb-4'>
-                    <label className='label p-2' htmlFor="username">
-                        <span className='text-base label-text text-gray-300'>Username</span>
-                    </label>
-                    <input
-                        id="username"
-                        type="text"
-                        placeholder="Type here"
-                        className="input input-bordered input-error w-full"
-                    />
-                </div>
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
 
-                <div className='mb-4'>
-                    <label className='label p-2' htmlFor="password">
-                        <span className='text-base label-text text-gray-300'>Password</span>
-                    </label>
-                    <input
-                        id="password"
-                        type="password"
-                        placeholder="Password"
-                        className="input input-bordered input-error w-full"
-                    />
-                </div>
+	const { loading, login } = useLogin();
 
-                <button type="submit" className='btn btn-primary w-full mt-4'>
-                    Log In
-                </button>
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		await login(username, password);
+	};
 
-                <Link to={"/signup"} className='text-sm text-gray-300 hover:underline hover:text-blue-500 mt-4 inline-block'>
-                    Don’t have an account?
-                </Link>
-            </form>
-        </div>
-    );
-}
+	return (
+		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+				<h1 className='text-3xl font-semibold text-center text-gray-300'>
+					Login
+					<span className='text-blue-500'> ChatApp</span>
+				</h1>
 
+				<form onSubmit={handleSubmit}>
+					<div>
+						<label className='label p-2'>
+							<span className='text-base label-text'>Username</span>
+						</label>
+						<input
+							type='text'
+							placeholder='Enter username'
+							className='w-full input input-bordered h-10'
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+						/>
+					</div>
+
+					<div>
+						<label className='label'>
+							<span className='text-base label-text'>Password</span>
+						</label>
+						<input
+							type='password'
+							placeholder='Enter Password'
+							className='w-full input input-bordered h-10'
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+					</div>
+					<Link to='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
+						{"Don't"} have an account?
+					</Link>
+
+					<div>
+						<button className='btn btn-block btn-sm mt-2' disabled={loading}>
+							{loading ? <span className='loading loading-spinner '></span> : "Login"}
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+};
 export default Login;
+
