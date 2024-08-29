3a4fcdf68927c9fb5f4fbefef361d04bc3a317cf
ajey108 <ajayxa1@gmail.com>
2024-08-29 16:24:24 +0530

    Update socketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 	useEffect(() => {
         //if there is auth user create a connection
 		if (authUser) {
-			const socket = io("https://localhost:5000", {
+			const socket = io("http://localhost:5000", {
 				query: {
 					userId: authUser._id,
 				},
